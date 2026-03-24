fd10a4b50e3036e2f766e024817100a846af4bd8
JuanPab2009 <pablo.blanco@iteso.mx>
2026-03-24 14:56:29 -0600

    Config de mongo y modelo alumno

--- tarea3/app.js
+++ tarea3/app.js
@@ -4,11 +4,23 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const methodOverride = require('method-override');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Handlebars
+// Conexión a MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  serverSelectionTimeoutMS: 5000
+})
+  .then(() => {
+    console.log('Conexión exitosa a MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error al conectar a MongoDB:', error.message);
+  });
+
+// Configurar Handlebars
 app.engine('hbs', engine({
   extname: '.hbs',
   defaultLayout: 'main'
